6bc0f6e24e6b8c46ad50c6ddec2b6de868745e28
Erick Contreras <eri89--@hotmail.com>
2021-08-03 19:00:35 -0500

    Add html content through javaScript

--- src/index.js
+++ src/index.js
@@ -73,22 +73,43 @@ function htmlContent() {
     } else {
       names.classList.add('names2');
     }
-    console.log(i)
   });
 
   // Section 2
   const section2 = document.createElement('section');
+  section2.id = 'add-score';
+
   const addYourScore = document.createElement('h2');
+  addYourScore.textContent = 'Add your score';
   // Form
   const form = document.createElement('form');
   form.setAttribute('action', 'https://formspree.io/f/mayajwdo');
   form.setAttribute('method', 'POST');
+
   const labelName = document.createElement('label');
+  labelName.setAttribute('for', 'name');
+
   const inputName = document.createElement('input');
+  inputName.setAttribute('type', 'text');
+  inputName.setAttribute('name', 'name');
+  inputName.id = 'name';
+  inputName.setAttribute("required", "");
+
   const labelScore = document.createElement('label');
+  labelName.setAttribute('for', 'score');
+
   const inputScore = document.createElement('input');
+  inputScore.setAttribute('type', 'text');
+  inputScore.setAttribute('name', 'score');
+  inputScore.id = 'score';
+  inputScore.setAttribute("required", "");
+
   const containerSubmitBtn = document.createElement('div');
+  containerSubmitBtn.id = 'submit-container';
+
   const submitBtn = document.createElement('input');
+  submitBtn.setAttribute('type', 'submit');
+  submitBtn.id = 'submit';
 
   //container appendChilds
   container.appendChild(header);
@@ -102,9 +123,15 @@ function htmlContent() {
   scoresTitle.appendChild(scoresTitleBtn);
   // Section 1 scores appendChild
   section1.appendChild(scores);
-
+  // Section 2 
+  section2.appendChild(addYourScore);
+  section2.appendChild(form);
+  form.appendChild(labelName);
+  form.appendChild(inputName);
+  form.appendChild(labelScore);
+  form.appendChild(inputScore);
+  form.appendChild(containerSubmitBtn);
+  containerSubmitBtn.appendChild(submitBtn);
 }
 
-// window.addEventListener('DOMContentLoaded', () => {
 htmlContent();
-// })
